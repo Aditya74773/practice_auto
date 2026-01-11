7a05df98b59be96d336797b68ee22c0ac44d9453
Aditya Kumar <adityakum2004@gmail.com>
2026-01-11 19:31:22 +0530

    added again

--- main.tf
+++ main.tf
@@ -3,15 +3,16 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS (Update for your region)
+  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS (us-east-1)
   instance_type = "t2.micro"
-  key_name      = "my-key"                # Your AWS Key Pair name
+  
+  # UPDATED: Using the correct Key Pair name from your AWS Console
+  key_name      = "Aadii_new"
 
   tags = {
     Name = "Jenkins-Grafana-Instance"
   }
 
-  # Allow SSH and Grafana port
   vpc_security_group_ids = [aws_security_group.allow_traffic.id]
 }
 
@@ -41,6 +42,7 @@ resource "aws_security_group" "allow_traffic" {
   }
 }
 
+# CRITICAL: This output block allows Jenkins to grab the IP
 output "instance_ip" {
   value = aws_instance.app_server.public_ip
 }
